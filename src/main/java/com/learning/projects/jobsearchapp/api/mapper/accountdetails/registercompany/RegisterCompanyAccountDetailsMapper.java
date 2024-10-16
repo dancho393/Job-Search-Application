8689c5fc4b50c358382a65437b90a71dc06d2ddf
dancho393 <danez7000@gmail.com>
2024-10-16 22:21:55 +0200

    added mapper interface for the current operation

---
+++ src/main/java/com/learning/projects/jobsearchapp/api/mapper/accountdetails/registercompany/RegisterCompanyAccountDetailsMapper.java
@@ -0,0 +1,45 @@
+package com.learning.projects.jobsearchapp.api.mapper.accountdetails.registercompany;
+
+import com.learning.projects.jobsearchapp.api.accountdetails.registercompany.RegisterCompanyRequest;
+import com.learning.projects.jobsearchapp.api.accountdetails.registercompany.RegisterCompanyResponse;
+import com.learning.projects.jobsearchapp.persistence.entity.AccountDetails;
+import com.learning.projects.jobsearchapp.persistence.entity.Company;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface RegisterCompanyAccountDetailsMapper {
+
+    @Mappings({
+            @Mapping(target = "id", source = "id"),
+            @Mapping(target = "username", source = "username"),
+            @Mapping(target = "name", source = "name"),
+            @Mapping(target = "address", source = "address"),
+            @Mapping(target = "phoneNumber", source = "phoneNumber"),
+            @Mapping(target = "city", source = "city"),
+            @Mapping(target = "employeeCount", source = "company.employeeCount"),
+            @Mapping(target = "rating", source = "company.rating"),
+            @Mapping(target = "message", constant = "Company registered successfully")
+    })
+    RegisterCompanyResponse toRegisterCompanyResponse(AccountDetails accountDetails);
+
+    @Mappings({
+            @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID())"),
+            @Mapping(target = "accountDetails", source = "accountDetails") ,
+            @Mapping(target = "employeeCount", source = "employeeCount"),
+            @Mapping(target = "rating", constant = "0.0f")
+    })
+    Company toCompany(int employeeCount, AccountDetails accountDetails);
+
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "username", source = "request.username")
+    @Mapping(target = "name", source = "request.name")
+    @Mapping(target = "address", source = "request.address")
+    @Mapping(target = "phoneNumber", source = "request.phoneNumber")
+    @Mapping(target = "city", source = "request.city")
+    @Mapping(target = "accountType", constant = "COMPANY")
+    @Mapping(target = "password", ignore = true)
+    AccountDetails toAccountDetails(RegisterCompanyRequest request);
+}
