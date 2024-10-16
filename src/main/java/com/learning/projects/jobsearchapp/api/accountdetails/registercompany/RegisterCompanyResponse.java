fbcdf266e0f17e0372ed324fe366aa184d7b1b08
dancho393 <danez7000@gmail.com>
2024-10-16 22:22:18 +0200

    changed response to return the created object and message for it's creation

--- src/main/java/com/learning/projects/jobsearchapp/api/accountdetails/registercompany/RegisterCompanyResponse.java
+++ src/main/java/com/learning/projects/jobsearchapp/api/accountdetails/registercompany/RegisterCompanyResponse.java
@@ -2,7 +2,17 @@ package com.learning.projects.jobsearchapp.api.accountdetails.registercompany;
 
 import com.learning.projects.jobsearchapp.api.base.OperationResponse;
 
+import java.util.UUID;
+
 public record RegisterCompanyResponse (
+         UUID id,
+         String username,
+         String name,
+         String address,
+         String phoneNumber,
+         String city,
+         int employeeCount,
+         Float rating,
         String message
 )implements OperationResponse
 {
