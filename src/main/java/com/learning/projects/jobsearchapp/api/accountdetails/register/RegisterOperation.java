069f8342a18b02ddd0dfc0b7c37d17d81610f764
dancho393 <danez7000@gmail.com>
2024-10-16 15:47:24 +0200

    register classes needed for testing

---
+++ src/main/java/com/learning/projects/jobsearchapp/api/accountdetails/register/RegisterOperation.java
@@ -0,0 +1,7 @@
+package com.learning.projects.jobsearchapp.api.accountdetails.register;
+
+import com.learning.projects.jobsearchapp.api.base.Operation;
+
+public interface RegisterOperation extends Operation<RegisterRequest,RegisterResponse> {
+
+}
---
+++ src/main/java/com/learning/projects/jobsearchapp/api/accountdetails/register/RegisterResponse.java
@@ -0,0 +1,9 @@
+package com.learning.projects.jobsearchapp.api.accountdetails.register;
+
+import com.learning.projects.jobsearchapp.api.base.OperationResponse;
+
+public record RegisterResponse(
+        String message
+) implements OperationResponse {
+
+}
