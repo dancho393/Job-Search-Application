6a2ba7aa8205034a8b128c43b1780dc186f56bf1
dancho393 <danez7000@gmail.com>
2024-10-17 19:47:08 +0200

    added the operation method(someone forgot to put it)

--- src/main/java/com/learning/projects/jobsearchapp/api/accountdetails/registeruser/RegisterUserOperation.java
+++ src/main/java/com/learning/projects/jobsearchapp/api/accountdetails/registeruser/RegisterUserOperation.java
@@ -2,6 +2,8 @@ package com.learning.projects.jobsearchapp.api.accountdetails.registeruser;
 
 import com.learning.projects.jobsearchapp.api.base.Operation;
 
-public interface RegisterUserOperation extends Operation<RegisterUserRequest,RegisterUserResponse> {
-
+public interface RegisterUserOperation
+        extends Operation<RegisterUserRequest,RegisterUserResponse> {
+    @Override
+    RegisterUserResponse process(RegisterUserRequest request);
 }
