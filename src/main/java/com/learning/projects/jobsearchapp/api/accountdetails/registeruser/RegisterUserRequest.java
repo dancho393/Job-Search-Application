1a2228fe82424d33e60057b0f53a7864bbfe7fe3
dancho393 <danez7000@gmail.com>
2024-10-23 23:33:28 +0200

    added validation for register endpoints

--- src/main/java/com/learning/projects/jobsearchapp/api/accountdetails/registeruser/RegisterUserRequest.java
+++ src/main/java/com/learning/projects/jobsearchapp/api/accountdetails/registeruser/RegisterUserRequest.java
@@ -1,14 +1,15 @@
 package com.learning.projects.jobsearchapp.api.accountdetails.registeruser;
 
 import com.learning.projects.jobsearchapp.api.base.OperationRequest;
+import jakarta.validation.constraints.NotEmpty;
 
 public record RegisterUserRequest(
-         String username,
-         String password,
-         String name,
-         String email,
-         String address,
-         String phoneNumber,
-         String city
+        @NotEmpty(message = "Username is required") String username,
+        @NotEmpty(message = "Password is required")  String password,
+        @NotEmpty(message = "Your Full Name Is Required")   String name,
+        @NotEmpty(message = "Your Email Is Required")  String email,
+        @NotEmpty(message = "Your Address Is Required")  String address,
+        @NotEmpty(message = "Your Phone Number Is Required")  String phoneNumber,
+        @NotEmpty(message = "Your City Is Required")  String city
 )implements OperationRequest {
 }
--- src/main/java/com/learning/projects/jobsearchapp/rest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/learning/projects/jobsearchapp/rest/exception/GlobalExceptionHandler.java
@@ -3,8 +3,14 @@ package com.learning.projects.jobsearchapp.rest.exception;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +23,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleEntityAlreadyExistsException(EntityAlreadyExistsException ex) {
 
         return new ResponseEntity<>("Entity already exists: " + ex.getMessage(), HttpStatus.CONFLICT);
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
